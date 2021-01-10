f8f12b8acadb751885f164c75c97471ba0b5f41f
Andrew Pieschke <ampieschke@gmail.com>
2021-01-09 22:31:37 -0500

    added and created a readme

--- Assets/main.js
+++ Assets/main.js
@@ -1,17 +1,11 @@
 var d = new Date();
-var timeBlockEl = document.querySelector("#time-block");
-// var hour_block = `<row class="row time-block" value="9">
-// <div class="col-2 hour">9am</div>
-// <textarea class="col-9 description past"></textarea>
-// <button class="col-1 saveBtn"><i class="fas fa-save"></i></button>
-// </row>`;
+var tasks = [];
 
 document.getElementById("currentDay").innerHTML = d;
-document.getElementById("demo").innerHTML = d.getHours();
 
-/* Needs to be able to show 9 hours within a work day. Each hour is displayed in a row. Each row contains an hour, a test array and a save button */
+/* Show 9 hours within a work day. Each hour is displayed in a row. Each row contains an hour, a test array and a save button */
 function show_hours() {
-  // Print out nine Hours
+  // Print out nine Hours with appropriate formating classes
   for (let hour = 9; hour < 18; hour++) {
     var current_hour = d.getHours();
     let hour_class;
@@ -23,24 +17,52 @@ function show_hours() {
       hour_class = "future";
     }
 
-    // Create a row
+    // Create the row elements and attached their hour values
     $("#schedule").append(`<row class="row time-block" value=${hour}>
         <div class="col-2 hour">${hour > 12 ? hour - 12 : hour}</div>
         <textarea class="col-9 description ${hour_class}"></textarea>
         <button class="col-1 saveBtn"><i class="fas fa-save"></i></button>
         </row>`);
+
+    //put local storage in this loop?
+    // if (hour == tasks.timeBlock) {
+    //   $(".description").html(lineItem);
+    //
   }
-  var lineItem = localStorage.getItem("text");
-  $(".description").html(lineItem);
+  renderLineItems();
 }
 
+// Will save line items to local storage
 function save() {
   let text = $(this).siblings(".description").val();
   let dayHour = $(this).siblings(".hour").text();
   console.log(text);
   console.log(dayHour);
+
   localStorage.setItem("text", text);
   localStorage.setItem("timeblock", dayHour);
+
+  let bingo = {
+    text: text,
+    timeBlock: dayHour,
+  };
+  tasks.push(bingo);
+
+  localStorage.setItem("lineItem", JSON.stringify(tasks));
+
+  console.log(tasks);
+}
+
+//Will call line items from local storage
+function renderLineItems() {
+  var lineItem = localStorage.getItem("text");
+  for (let hour = 9; hour < 18; hour++) {
+    if (hour == dayHour) {
+      $(this).siblings(".desciption").html(lineItem.text);
+    }
+  }
+  //   //Prints the last lineItem value to all rows
+  //   $(".description").html(lineItem);
 }
 
 $("#schedule").on("click", ".saveBtn", save);
